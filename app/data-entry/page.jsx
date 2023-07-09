d616ba21422611c2cf7de42059ba75a20b409707
Kim Wong <15217035@life.hkbu.edu.hk>
2023-07-09 00:35:38 -0400

    add form components to data entry page

--- app/data-entry/page.jsx
+++ app/data-entry/page.jsx
@@ -6,6 +6,7 @@ export const metadata = {
 
 import React from "react"
 import Header from "@/components/common/Header"
+import FormComponent from "@/components/form/FormComponent"
 
 const Page = () => {
   const sectionMessage = {
@@ -14,12 +15,15 @@ const Page = () => {
   }
 
   return (
-    <div>
-      <Header
-        title={sectionMessage.title}
-        description={sectionMessage.paragraph}
-      />
-    </div>
+    <>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
+        <Header
+          title={sectionMessage.title}
+          description={sectionMessage.paragraph}
+        />
+        <FormComponent />
+      </div>
+    </>
   )
 }
 
