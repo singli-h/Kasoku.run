00e32eb61721de92c86bebf65c6d3968fa2a86a8
sing-getproofed <101650960+sing-getproofed@users.noreply.github.com>
2024-12-05 16:24:54 +0000

    update

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const Header = ({ title, description }) => (
-  <div className="max-w-3xl mx-auto">
+  <div className="max-w-6xl mx-auto">
     <h1 className="h1 mb-4">{title}</h1>
     <p className="text-xl text-gray-400 mb-8">{description}</p>
   </div>
